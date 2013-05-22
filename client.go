06bbf5fdd2404a87778e85e3f810e0ca6c50974e
Clark Gaebel <cg.wowus.cg@gmail.com>
2013-05-21 20:11:30 -0400

    Walls!

--- client.go
+++ client.go
@@ -1,7 +1,38 @@
 package main
 
+import "github.com/nsf/termbox-go"
+
+const (
+    width = 80
+    height = 24
+)
+
 type Client struct {
 }
 
+func drawWallPanel(x, y int) {
+    termbox.SetCell(x, y, '#', termbox.ColorBlack, termbox.ColorDefault)
+}
+
+func drawVertWalls() {
+    for i := 0; i < height; i++ {
+        drawWallPanel(0, i)
+        drawWallPanel(width, i)
+    }
+}
+
+func drawHorizWalls() {
+    for i := 0; i < width; i++ {
+        drawWallPanel(i, 0)
+        drawWallPanel(i, height)
+    }
+}
+
+func drawGrid() {
+    drawVertWalls()
+    drawHorizWalls()
+}
+
 func (client *Client) Tick() {
+    drawGrid()
 }
