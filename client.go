16dce395848134f56aeac1c525092bc1a06986f6
Jason Moiron <jmoiron@jmoiron.net>
2013-05-21 20:17:15 -0400

    gofmt

--- client.go
+++ client.go
@@ -3,36 +3,36 @@ package main
 import "github.com/nsf/termbox-go"
 
 const (
-    width = 80
-    height = 24
+	width  = 80
+	height = 24
 )
 
 type Client struct {
 }
 
 func drawWallPanel(x, y int) {
-    termbox.SetCell(x, y, '#', termbox.ColorBlack, termbox.ColorDefault)
+	termbox.SetCell(x, y, '#', termbox.ColorWhite, termbox.ColorDefault)
 }
 
 func drawVertWalls() {
-    for i := 0; i < height; i++ {
-        drawWallPanel(0, i)
-        drawWallPanel(width, i)
-    }
+	for i := 0; i < height; i++ {
+		drawWallPanel(0, i)
+		drawWallPanel(width, i)
+	}
 }
 
 func drawHorizWalls() {
-    for i := 0; i < width; i++ {
-        drawWallPanel(i, 0)
-        drawWallPanel(i, height)
-    }
+	for i := 0; i < width; i++ {
+		drawWallPanel(i, 0)
+		drawWallPanel(i, height)
+	}
 }
 
 func drawGrid() {
-    drawVertWalls()
-    drawHorizWalls()
+	drawVertWalls()
+	drawHorizWalls()
 }
 
 func (client *Client) Tick(event termbox.Event) {
-    drawGrid()
+	drawGrid()
 }
